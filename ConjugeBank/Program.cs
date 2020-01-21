a04fc9214ad821cd15557d5bca28a5690ca86a4d
Santi L <idlucassanti@hotmail.com>
2020-01-21 01:14:31 -0300

    :sparkles: filling an object

--- ConjugeBank/Program.cs
+++ ConjugeBank/Program.cs
@@ -8,6 +8,11 @@ namespace ConjugeBank
         {
             ContaCorrente conta = new ContaCorrente();
 
+            conta.titular = "Lucas Santi";
+            conta.numeroAgencia = 321;
+            conta.numeroConta = 70040;
+            conta.saldo = 1000.0;
+
             Console.ReadLine();
         }
     }
